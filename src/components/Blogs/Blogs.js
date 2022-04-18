644c3c644fa0f653931bbfc4a348740f19ac2930
motalibpathan11 <73628059+motalibpathan11@users.noreply.github.com>
2022-04-18 17:35:23 +0600

    question answer added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,17 +2,29 @@ import React from "react";
 
 const Blogs = () => {
   return (
-    <div>
-      <h1 className="text-xl font-bold">
+    <div className="md:container mx-auto p-5 min-h-screen">
+      <h1 className="my-3 text-xl font-bold">
         Question: Why are you using firebase? What other options do you have to
         implement authentication?
       </h1>
-      <p>Answer: </p>
-      <h1 className="text-xl font-bold">
+      <p>
+        Answer: Firebase helps to authenticate user in a website. We can
+        implement sing in with firebase authentication service. By Firebase
+        authentication service we can create account with email password,
+        google, facebook, github, microsoft, apple account. We dont have to
+        manage user authentication because by firebase we can authenticate user
+        easily. The alternative way to implementation authentication we can use
+        Auth0, Okta, Amazon Cognito, Keycloak, JSON Web Token etc.
+      </p>
+      <h1 className="my-3 text-xl font-bold">
         Question: What other services does firebase provide other than
         authentication
       </h1>
-      <p>Answer: </p>
+      <p>
+        Answer: The other services does firebase provide other than
+        authentication are Cloud Firestore, Hosting, Cloud Storage,Google
+        Analytics, Predictions, Cloud Messaging, Realtime Database etc.
+      </p>
     </div>
   );
 };
